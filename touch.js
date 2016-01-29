83a57c3b1a2eb6eb55f17debe57665713dfbcb9d
周鹏 <1179258516@qq.com>
2016-01-30 03:28:20 +0800

    手机滑动事件

---
+++ touch.js
@@ -0,0 +1,43 @@
+var Touch = function (keynum, callback) {
+	var startX,
+		startY,
+		disX,
+		disY,
+		hasTouched = 'ontouchstart' in window,
+		touchstart = hasTouched ? 'touchstart' : 'mousedown',
+		touchmove = hasTouched ? 'touchmove' : '',
+		touchend = hasTouched ? 'touchend' : 'mouseup',
+		slideW = document.body.clientWidth,
+		slideH = document.body.clientHeight;
+	function Start (e) {
+		var point = hasTouched ? e.touches[0] : e;
+		startX = point.pageX;
+		startY = point.pageY;
+		document.addEventListener(touchmove, Move, false);
+		document.addEventListener(touchend, End, false);
+	}
+	function Move (e) {
+		e.preventDefault();
+		if (hasTouched) {
+			if (e.touches.length > 1) return;
+		}
+		var point = hasTouched ? e.touches[0] : e;
+		disX = point.pageX - startX;
+		disY = point.pageY - startY;
+	}
+	function End (e) {
+		e.preventDefault();
+		var	x = Math.abs(disX),
+			y = Math.abs(disY);
+		if (x < slideW / 10 && y < slideH / 50) return;
+		if (x > y) {
+			keynum = disX > 0 ? 39 : 37;
+		} else {
+			keynum = disY > 0 ? 40 : 38;
+		}
+		document.removeEventListener(touchmove, Move, false);
+		document.removeEventListener(touchend, End, false);
+		callback(keynum);
+	}
+	document.addEventListener(touchstart, Start, false);
+}
